21c1f3d8f2be640d32def894f6400dcbb20239d1
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2025-01-15 23:03:41 +0600

    list

---
+++ src/Dashboard/AssociativeList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AssociativeList = () => {
+  return <div></div>;
+};
+
+export default AssociativeList;
---
+++ src/Dashboard/ExecutiveList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExecutiveList = () => {
+  return <div></div>;
+};
+
+export default ExecutiveList;
